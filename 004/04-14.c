88048f81e6a61996f861600cadb0dc42e8dbdd5a
mult <sergeyartemenko@gmail.com>
2016-10-06 12:45:44 +0700

    1.0 сделано через stdarg.h

--- 004/04-14.c
+++ 004/04-14.c
@@ -1,106 +1,138 @@
 // Лабораторная работа 4
-// Разработать функцию с переменным количеством параметров. Для извлечения параметров из списка использовать
-// технологию программирования областей памяти переменного формата, описанную в 4.4.
 
-// 14. Первый параметр - строка, в которой каждый символ «*n», где n-цифра -  обозначает место включения целого (int), 
-// являющегося n+1 параметром. Функция выводит на экран полученный текст, содержащий целые значения.
+#include <stdio.h>      //  printf(), fwrite(), fflush(), stdout
+#include <stdlib.h>     //  malloc(), free(), mblen()
+#include <ctype.h>      //  isdigit()
+#include <stdarg.h>     //  va_start(), va_end(), va_arg()
+#include <locale.h>     //  setlocale(LC_ALL, "ru_RU.utf8")
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+// решение лабораторной работы.
+// принимает:   форматную строку и
+//              несколько переменных типа int зависимости от содержимого строки.
+void VA_Show(char*, ...);
 
-void Print(char* str, ...);
-int printUTFchar(char* ch);
-char* IntToStr(char* str, int num);
+//выводит много байтовый символ в файл
+//принимает:    указатель на первый байт символа
+//возвращает:   длину выведенного символа или
+//              -1 если символ не соответствует категории LC_CTYPE
+int fputUTFchar(char*, FILE*);
+
+// преобразует массив символов содержащий внешнее представление числа
+// во внутреннее представление
+// принимает:   указатель на массив символов
+//              длину массива символов
+// возвращает:  число во внутреннем представлении.
+// НЕ КОНТРОЛИРУЕТ СОДЕРЖАНИЕ МАССИВА СИМВОЛОВ!!!!!
+// НЕ ОБРАБАТЫВАЕТ ЗНАК '-'
+int my_atoi(char* ch, int len);
 
 int main(int argc, char const *argv[])
 {
-    char str[] = "as *1 df *0 sd";
-    int a = 1;
-    int b = 2;
-    Print(str, a, b);
+    setlocale(LC_ALL, "ru_RU.utf8");
+
+    VA_Show("as *2 df *0 sd", 1, 2, 3);
+
     return 0;
 }
 
-void Print(char* str, ...)
+void VA_Show(char* str, ...)
 {
-    char buf[25] = {0};
-    char tmpchar;
-    char* p = str;
-    char* s = str;
-    while (*(p++)!='\0') printf("%p %c %d\n",p,*p,(unsigned char)*p );
-    for (char i = 0; i < 16; ++i)
-    {
-        printf("%p %c %d\n",p+i,*(p+i),(unsigned char)*(p+i) );
-    }
-
-    int* A = (int*)p;
-    // int* A = &a;
-
+    int max_num=0;
     int isNum = 0;
-    for (p=str; *p!='\0'; p++)
+    char* p = str;
+
+    // ищем максимальное число написанное после символа '*'
+    while(*p!='\0')
     {
         if (isNum)
         {
-            printf(" is Num %s\n", p);
             if (isdigit(*p))
             {
                 char* t = p;
-                while(isdigit(*t)) printf("t-> %c\n", *t), t++;
-                tmpchar = *t;
-                printf("|%c|\n", tmpchar);
-                *t = '\0';
-                isNum = atoi(p);
-                // if (isNum<0)
-                //     strcpy(buf, " ERROR ");
-                // else
-                //     IntToStr(buf, A[isNum]);
-                printf("A %d %X\n", isNum, (unsigned int)A[0]);
-                puts(buf);
-                *t = tmpchar;
-                p = t-1;
+                while(isdigit(*t)) t++;
+                isNum = my_atoi(p, t-p);
+                if (isNum > max_num)
+                    max_num = isNum;
+                p = t;
             }
             else
-                p += printUTFchar(p)-1; 
+                p++;; 
             isNum = 0;
         }
         else
         {
-            printf(" is not Num %s\n", p);
             if (*p=='*')
                 isNum = 1;
-            else
-                p += printUTFchar(p)-1;
+            p++;
         }
-    }
-}
+    } 
 
-int printUTFchar(char* ch)
-{
-    
-    int charlen = mblen(ch, 6);
-    char buf[7];
-    // char t = ch[charlen];
-    // ch[charlen] = '\0';
-    // puts(ch);
-    // ch[charlen] = t;
-    memcpy(buf, ch, charlen);
-    buf[charlen]='\0';
-    puts(buf);
-    return charlen;
-}
+    // для организации произвольного доступа копируем все
+    // не обязательные параметры в массив.
+    int * A = (int*)malloc(sizeof(int)*max_num+1);
+    va_list ap;
+    va_start(ap, str);
+    for (int i = 0; i <= max_num; ++i)
+        A[i] = va_arg(ap, int);
+    va_end(ap);
 
-char* IntToStr(char* str, int num)
-{
-    int l = 1;
-    char* p = str;
-    for (int i = num; i > 10; i/=10, l*=10);
-    for  (; l>0 ; l/=10)
+
+    // выводим строку заменяя "*n" на n+1 параметр функции.
+    p = str;
+    isNum = 0;
+    while(*p!='\0')
     {
-        *(p++) = num/l+'0';
-         num=num%l;
+        if (isNum)
+        {
+            if (isdigit(*p))
+            {
+                char* t = p;
+                while(isdigit(*t)) t++;
+                isNum = my_atoi(p, t-p);
+                printf("%d", A[isNum]);
+                p = t;
+            }
+            else
+                p += fputUTFchar(p, stdout); 
+            isNum = 0;
+        }
+        else
+        {
+            if (*p=='*')
+            {
+                isNum = 1;
+                p++;
+            }
+            else
+                p += fputUTFchar(p, stdout);
+        }
+    } 
+    printf("\n");
+    free(A);
+}
+
+int my_atoi(char* ch, int len)
+{
+    char* end = ch + len;
+    int result = 0;
+    int osn = 10;
+    for (; ch<end ; ch++)
+    {
+        result *= osn;
+        result += (*ch-'0');
     }
-    *p = '\0';
-    return str;
+    return result;
+}
+
+int fputUTFchar(char* ch, FILE* F)
+{
+    int len = mblen(ch, 6);
+    char* end = ch + len;
+    while (ch < end)
+    {
+        fwrite(ch, sizeof(char), 1, F);
+        ch++;
+    }
+    fflush(F);
+    return len;
 }
